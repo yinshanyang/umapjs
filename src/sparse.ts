7fe233dad02c0ec31d05f073720aaa4e4d9118b5
Yin Shanyang <me@yinshanyang.com>
2018-05-21 01:14:42 +0800

    feature [wip]: sparse functions

---
+++ src/sparse.ts
@@ -0,0 +1,54 @@
+import * as nj from 'numjs'
+import * as utils from './utils'
+
+function unique () {}
+
+function union () {}
+
+function intersect () {}
+
+function sum () {}
+
+function diff () {}
+
+function mul () {}
+
+export function randomProjectionCosineSplit () {}
+
+export function randomProjectionSplit () {}
+
+export function makeNnDescent () {}
+
+export function euclidean () {}
+
+export function manhattan () {}
+
+export function chebyshev () {}
+
+export function minkowski () {}
+
+export function hamming () {}
+
+export function canberra () {}
+
+export function brayCurtis () {}
+
+export function jaccard () {}
+
+export function matching () {}
+
+export function dice () {}
+
+export function kulsinski () {}
+
+export function rogersTanimoto () {}
+
+export function russellrao () {}
+
+export function sokalMichener () {}
+
+export function sokalSneath () {}
+
+export function cosine () {}
+
+export function correlation () {}
