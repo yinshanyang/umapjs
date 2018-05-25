6ff7e4150dd9862b1b073ff68ee9843cf87154d8
Yin Shanyang <yang@swarm.is>
2018-05-25 17:39:02 +0800

    feature [wip]: stub validation functions

---
+++ src/validation.ts
@@ -0,0 +1,8 @@
+import * as nj from 'numjs'
+import * as distances from './distances'
+
+export function trustworthinessVectorBulk () {}
+
+export function makeTrustworthinessCalculator () {}
+
+export function trustworthinessVector () {}
